18f1b348d45191923322850a8572d043ad5544a5
Pedro Candido <134300577+PeCandido@users.noreply.github.com>
2024-10-04 19:25:58 -0300

    Update ApiApplication.java

--- avaliacao3_api/src/main/java/pedro/matheus/avaliacao3_api/ApiApplication.java
+++ avaliacao3_api/src/main/java/pedro/matheus/avaliacao3_api/ApiApplication.java
@@ -1,4 +1,6 @@
 package pedro.matheus.avaliacao3_api;
+// PEDRO CANDIDO SALVIO - SC3037126
+// MATHEUS HENRIQUE DE SOUZA CARVALHO - SC3037801
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
